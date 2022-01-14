5564392935ab910a8da30add112c36c1ee4a838e
ArnauDivins1 <arnau.divins.2173@lacetania.cat>
2022-01-14 16:21:23 +0100

    PRIMERA PUJADA

---
+++ EXERCICIS ARDUINO/DISPLAY DE 7 SEGMENTS/2_LED_DE_7_SEGMENTS/2_LED_DE_7_SEGMENTS/2_LED_DE_7_SEGMENTS.ino
@@ -0,0 +1,126 @@
+const byte segA = 5;          // donar nom al pin 5 de l’Arduino
+const byte segB = 6;          
+const byte segC = 7;          
+const byte segD = 8;          
+const byte segE = 9;          
+const byte segF = 10;         
+const byte segG = 11; 
+
+
+//********** Setup ****************************************************************
+void setup()
+{
+  pinMode(segA, OUTPUT);     
+  pinMode(segB, OUTPUT);     
+  pinMode(segC, OUTPUT);     
+  pinMode(segD, OUTPUT);     
+  pinMode(segE, OUTPUT);     
+  pinMode(segF, OUTPUT);     
+  pinMode(segG, OUTPUT);         
+}
+
+//********** Loop *****************************************************************
+void loop()
+{
+  {
+  digitalWrite(segA, HIGH);
+  digitalWrite(segB, HIGH);
+  digitalWrite(segC, HIGH);
+  digitalWrite(segD, HIGH);
+  digitalWrite(segE, HIGH);
+  digitalWrite(segF, HIGH);
+  digitalWrite(segG, LOW);
+
+  delay (1000);
+
+  digitalWrite(segA, LOW);
+  digitalWrite(segB, HIGH);
+  digitalWrite(segC, HIGH);
+  digitalWrite(segD, LOW);
+  digitalWrite(segE, LOW);
+  digitalWrite(segF, LOW);
+  digitalWrite(segG, LOW);
+  
+  delay (1000);
+
+  digitalWrite(segA, HIGH);
+  digitalWrite(segB, HIGH);
+  digitalWrite(segC, LOW);
+  digitalWrite(segD, HIGH);
+  digitalWrite(segE, HIGH);
+  digitalWrite(segF, LOW);
+  digitalWrite(segG, HIGH);
+
+  delay (1000);
+
+  digitalWrite(segA, HIGH);
+  digitalWrite(segB, HIGH);
+  digitalWrite(segC, HIGH);
+  digitalWrite(segD, HIGH);
+  digitalWrite(segE, LOW);
+  digitalWrite(segF, LOW);
+  digitalWrite(segG, HIGH);
+
+  delay (1000);
+
+  digitalWrite(segA, LOW);
+  digitalWrite(segB, HIGH);
+  digitalWrite(segC, HIGH);
+  digitalWrite(segD, LOW);
+  digitalWrite(segE, LOW);
+  digitalWrite(segF, HIGH);
+  digitalWrite(segG, HIGH);
+
+  delay (1000);
+
+  digitalWrite(segA, HIGH);
+  digitalWrite(segB, LOW);
+  digitalWrite(segC, HIGH);
+  digitalWrite(segD, HIGH);
+  digitalWrite(segE, LOW);
+  digitalWrite(segF, HIGH);
+  digitalWrite(segG, HIGH);
+
+  delay (1000);
+
+  digitalWrite(segA, LOW);
+  digitalWrite(segB, LOW);
+  digitalWrite(segC, HIGH);
+  digitalWrite(segD, HIGH);
+  digitalWrite(segE, HIGH);
+  digitalWrite(segF, HIGH);
+  digitalWrite(segG, HIGH);
+
+  delay (1000);
+
+  digitalWrite(segA, HIGH);
+  digitalWrite(segB, HIGH);
+  digitalWrite(segC, HIGH);
+  digitalWrite(segD, LOW);
+  digitalWrite(segE, LOW);
+  digitalWrite(segF, LOW);
+  digitalWrite(segG, LOW);
+
+  delay (1000);
+
+  digitalWrite(segA, HIGH);
+  digitalWrite(segB, HIGH);
+  digitalWrite(segC, HIGH);
+  digitalWrite(segD, HIGH);
+  digitalWrite(segE, HIGH);
+  digitalWrite(segF, HIGH);
+  digitalWrite(segG, HIGH);
+
+  delay (1000);
+
+  digitalWrite(segA, HIGH);
+  digitalWrite(segB, HIGH);
+  digitalWrite(segC, HIGH);
+  digitalWrite(segD, LOW);
+  digitalWrite(segE, LOW);
+  digitalWrite(segF, HIGH);
+  digitalWrite(segG, HIGH);
+
+  delay (1000);
+  }
+}
